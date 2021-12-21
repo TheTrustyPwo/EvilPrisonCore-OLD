73e1d41971121b31ed37e209d6397fac23705193
steeldragon960 <68459550+steeldragon960@users.noreply.github.com>
2021-12-21 22:59:50 +0800

    Credits updates

--- src/main/java/me/pwo/evilprisoncore/credits/gui/CreditsGUI.java
+++ src/main/java/me/pwo/evilprisoncore/credits/gui/CreditsGUI.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 
 public class CreditsGUI extends Gui {
     public CreditsGUI(Player player) {
-        super(player, 4, Text.colorize("&8Credits"));
+        super(player, 3, Text.colorize("&8Credits"));
     }
     @Override
     public void redraw() {
@@ -23,27 +23,52 @@ public class CreditsGUI extends Gui {
                 setItem(slot, ItemStackBuilder.of(Material.STAINED_GLASS_PANE).data(7).buildItem().build());
         // Season Age Info
         setItem(10, ItemStackBuilder.of(Material.BOOK)
-                .name("&6&lSeason Age: &e&l" + Credits.getInstance().getCreditsManager().getSeasonAge())
+                .name("&6Season Age: &e" + Credits.getInstance().getCreditsManager().getSeasonAge())
                 .lore(Arrays.asList(
-
+                        "&7OP Prison Season II released on",
+                        "&f9 Jan 2022 &7and will end on &f20 Feb 2022&7."
                 )).enchant(Enchantment.DURABILITY).flag(ItemFlag.HIDE_ENCHANTS).buildItem().build());
         // Your Credits Balance
         setItem(13, ItemStackBuilder.of(SkullUtils.getPlayerHead(getPlayer()))
-                .name("&6&lYour Credits: &4☀&c%credits%".replaceAll("%credits%", String.valueOf(Credits.getInstance().getCreditsManager().getPlayerCredits(getPlayer()))))
+                .name("&6Your Credits: &4☀&c%credits%".replaceAll("%credits%", String.valueOf(Credits.getInstance().getCreditsManager().getPlayerCredits(getPlayer()))))
                 .lore(Arrays.asList(
-
+                        "&7Credits can be exchanged for store credit!",
+                        "&7Type &f/credits withdraw <amount>",
+                        "&7to exchange them! You have withdraw to",
+                        "&7a minimum of &f100,000 &7credits."
                 )).buildItem().build());
         // Credits Multiplier
         setItem(16, ItemStackBuilder.of(Material.BOOK)
-                .name("&6&lCredits Multi: &e&lx" + Credits.getInstance().getCreditsManager().getPayoutMultiplier())
+                .name("&6Credits Multi: &e&lx" + Credits.getInstance().getCreditsManager().getPayoutMultiplier())
                 .lore(Arrays.asList(
-
-                        )).enchant(Enchantment.DURABILITY).flag(ItemFlag.HIDE_ENCHANTS).buildItem().build());
+                        "&7As the season ages, the credits",
+                        "&7multiplier will increase! This means you will be",
+                        "&7able to trade credits for more store",
+                        "&7credit during late season. It will",
+                        "&7get as high as &fx10.50 &7at the",
+                        "&7end of the season!"
+                )).enchant(Enchantment.DURABILITY).flag(ItemFlag.HIDE_ENCHANTS).buildItem().build());
         // Help Item
-        setItem(31, ItemStackBuilder.of(Material.BOOK_AND_QUILL)
-                .name("&eHow to get &4☀&c%Credits&e?")
+        setItem(22, ItemStackBuilder.of(Material.BOOK_AND_QUILL)
+                .name("&eInformation")
                 .lore(Arrays.asList(
-
+                        "&6What are credits?",
+                        "&7Credits are a custom currency on our server",
+                        "&7which you can exchange for store credit to buy",
+                        "&7Ranks, Crates, Special items and abilities etc...",
+                        " ",
+                        "&6How to earn credits?",
+                        "&7Now, here's the exciting part: ANYONE can earn credits!",
+                        "&7There are multiple ways of earning credits, such as",
+                        "&7Mining, AutoMining, Ranks, Robots, Events, PvP and more!",
+                        "&7This makes it extremely fair for ALL players as",
+                        "&7you can't just P2W your way for credits.",
+                        " ",
+                        "&6What to do with credits",
+                        "&7Type &f/credits withdraw <amount> &7to exchange them!",
+                        "&7Currently, you can only exchange them into gift cards, but some",
+                        "&7day in the future, when our server gets big enough,",
+                        "&7you guys will even be able to withdraw them into Paypal!"
                 )).enchant(Enchantment.DURABILITY).flag(ItemFlag.HIDE_ENCHANTS).buildItem().build());
     }
 }
